eb1f11d0cd203a4f5ebfee5eb6873b27906d2d71
bsetman <969110018@qq.com>
2025-05-26 19:04:25 +0300

    Update F_WORKS_LIST_OPTIMIZED.sql

--- F_WORKS_LIST_OPTIMIZED.sql
+++ F_WORKS_LIST_OPTIMIZED.sql
@@ -1,78 +1,73 @@
-CREATE FUNCTION [dbo].[F_WORKS_LIST]()
+ALTER FUNCTION [dbo].[F_WORKS_LIST] (
+)
 RETURNS @RESULT TABLE
 (
-    ID_WORK INT,
-    CREATE_Date DATETIME,
-    MaterialNumber DECIMAL(8,2),
-    IS_Complit BIT,
-    FIO VARCHAR(255),
-    D_DATE VARCHAR(10),
+    ID_WORK             INT,
+    CREATE_Date         DATETIME,
+    MaterialNumber      DECIMAL(8,2),
+    IS_Complit          BIT,
+    FIO                 VARCHAR(255),
+    D_DATE              VARCHAR(10),
     WorkItemsNotComplit INT,
-    WorkItemsComplit INT,
-    FULL_NAME VARCHAR(101),
-    StatusId SMALLINT,
-    StatusName VARCHAR(255),
-    Is_Print BIT
+    WorkItemsComplit    INT,
+    FULL_NAME           VARCHAR(101),
+    StatusId            SMALLINT,
+    StatusName          VARCHAR(255),
+    Is_Print            BIT
 )
 AS
 BEGIN
-    WITH TopWorks AS (
-        SELECT TOP (3000)
-            Id_Work,
-            CREATE_Date,
-            MaterialNumber,
-            IS_Complit,
-            FIO,
-            Id_Employee,
-            StatusId,
-            Print_Date,
-            SendToClientDate,
-            SendToDoctorDate,
-            SendToOrgDate,
-            SendToFax
-        FROM Works
-        WHERE Is_Del <> 1
-        ORDER BY Id_Work DESC
-    ),
-    WorkItemAgg AS (
-        SELECT
-            WI.Id_Work,
-            SUM(CASE WHEN WI.Is_Complit = 0 THEN 1 ELSE 0 END) AS WorkItemsNotComplit,
-            SUM(CASE WHEN WI.Is_Complit = 1 THEN 1 ELSE 0 END) AS WorkItemsComplit
-        FROM WorkItem WI
-        JOIN Analiz A ON WI.ID_ANALIZ = A.ID_ANALIZ AND A.IS_GROUP = 0
-        WHERE WI.Id_Work IN (SELECT Id_Work FROM TopWorks)
-        GROUP BY WI.Id_Work
-    )
     INSERT INTO @RESULT
     SELECT
-        TW.Id_Work,
-        TW.CREATE_Date,
-        TW.MaterialNumber,
-        TW.IS_Complit,
-        TW.FIO,
-        CONVERT(VARCHAR(10), TW.CREATE_Date, 104) AS D_DATE,
-        ISNULL(WIA.WorkItemsNotComplit, 0) AS WorkItemsNotComplit,
-        ISNULL(WIA.WorkItemsComplit, 0) AS WorkItemsComplit,
-        ISNULL(
-            E.Surname + ' ' + UPPER(LEFT(E.Name, 1)) + '. ' + UPPER(LEFT(E.Patronymic, 1)) + '.',
-            E.Login_Name
+        w.Id_Work,
+        w.CREATE_Date,
+        w.MaterialNumber,
+        w.IS_Complit,
+        w.FIO,
+        
+        CONVERT(VARCHAR(10), w.CREATE_Date, 104) AS D_DATE,
+        
+        ISNULL(wi_counts.WorkItemsNotComplit, 0) AS WorkItemsNotComplit,
+        ISNULL(wi_counts.WorkItemsComplit,    0) AS WorkItemsComplit,
+        
+        RTRIM(
+          e.Surname
+          + ' ' + LEFT(e.Name,1) + '.'
+          + ' ' + LEFT(e.Patronymic,1) + '.'
         ) AS FULL_NAME,
-        TW.StatusId,
-        WS.StatusName,
+        w.StatusId,
+        s.StatusName,
+        
         CASE
-            WHEN TW.Print_Date IS NOT NULL
-                OR TW.SendToClientDate IS NOT NULL
-                OR TW.SendToDoctorDate IS NOT NULL
-                OR TW.SendToOrgDate IS NOT NULL
-                OR TW.SendToFax IS NOT NULL
-            THEN 1 ELSE 0
+          WHEN w.Print_Date       IS NOT NULL
+            OR w.SendToClientDate IS NOT NULL
+            OR w.SendToDoctorDate IS NOT NULL
+            OR w.SendToOrgDate    IS NOT NULL
+            OR w.SendToFax        IS NOT NULL
+          THEN 1 ELSE 0
         END AS Is_Print
-    FROM TopWorks TW
-    LEFT JOIN WorkItemAgg WIA ON TW.Id_Work = WIA.Id_Work
-    LEFT JOIN Employee E ON E.Id_Employee = TW.Id_Employee
-    LEFT JOIN WorkStatus WS ON WS.StatusID = TW.StatusId
-    ORDER BY TW.Id_Work DESC;
+    FROM
+        Works w
+        LEFT JOIN WorkStatus s
+          ON s.StatusID = w.StatusId
+        LEFT JOIN Employee e
+          ON e.Id_Employee = w.Id_Employee
+        LEFT JOIN (
+            
+            SELECT
+              wi.Id_Work,
+              SUM(CASE WHEN wi.Is_Complit = 0 AND a.Is_Group = 0 THEN 1 ELSE 0 END) AS WorkItemsNotComplit,
+              SUM(CASE WHEN wi.Is_Complit = 1 AND a.Is_Group = 0 THEN 1 ELSE 0 END) AS WorkItemsComplit
+            FROM WorkItem wi
+            INNER JOIN Analiz a
+              ON wi.Id_Analiz = a.Id_Analiz
+            GROUP BY wi.Id_Work
+        ) AS wi_counts
+          ON wi_counts.Id_Work = w.Id_Work
+    WHERE
+        w.Is_Del <> 1
+    ORDER BY
+        w.Id_Work DESC;
 
     RETURN;
 END
